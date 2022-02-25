c4ff65cf7591b6bae1997281387439513a5f071d
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-02-25 15:01:23 +0800

    Update ch7_lab.sh

--- DO180/ch7_lab.sh
+++ DO180/ch7_lab.sh
@@ -1,7 +1,7 @@
 # lab multicontainer-review start
 cd ~/DO180/labs/multicontainer-review
-oc login -u ${RHT_OCP4_DEV_USER} \
--p ${RHT_OCP4_DEV_PASSWORD} ${RHT_OCP4_MASTER_API}
+# oc login -u ${RHT_OCP4_DEV_USER} \
+# -p ${RHT_OCP4_DEV_PASSWORD} ${RHT_OCP4_MASTER_API}
 oc new-project \
 ${RHT_OCP4_DEV_USER}-deploy
 podman login registry.redhat.io
