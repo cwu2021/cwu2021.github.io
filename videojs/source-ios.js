8a2dcdcb8a1c42711b0a606b86168fc349fd0884
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-05-13 00:15:34 +0800

    simply display source code on mobile safari.

---
+++ videojs/source-ios.js
@@ -0,0 +1,2 @@
+// https://appletoolbox.com/how-to-view-webpage-html-source-codes-on-ipad-iphone-no-app-required/
+javascript:(function(){var a=window.open('about:blank').document;a.write('<!DOCTYPE html><html><head><title>Source of '+location.href+'</title><meta name="viewport" content="width=device-width" /></head><body></body></html>');a.close();var b=a.body.appendChild(a.createElement('pre'));b.style.overflow='auto';b.style.whiteSpace='pre-wrap';b.appendChild(a.createTextNode(document.documentElement.innerHTML))})();
