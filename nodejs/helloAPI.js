f70456541cdb3ca7027ed7217519c1ad52cea7dd
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-07-27 10:32:28 +0800

    a simple "hello world" API call

---
+++ nodejs/helloAPI.js
@@ -0,0 +1,12 @@
+// https://opensource.com/article/22/7/javascript-api-express
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
