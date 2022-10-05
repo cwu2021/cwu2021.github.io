87bdd48d33f2a4b42328c239f84535ac050e6fc5
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2022-10-05 10:18:30 +0800

    serve running.json locally with httpd

--- JSON-URL-to-HTML-Table/fetch_election2022.sh
+++ JSON-URL-to-HTML-Table/fetch_election2022.sh
@@ -2,4 +2,5 @@
 cd ~/json
 wget --no-check-certificate --http-user=user --http-password=password https://download.2022dl.nat.gov.tw/test/running.json
 # conversion from running.json to votes.json
-# sync votes.json to cloud for web with CORS allowed. serve locally with httpd.
+# sync votes.json to cloud for web with CORS allowed. serve running.json locally with httpd.
+# ln -s ~/json /var/www/html
